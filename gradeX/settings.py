c3c48d4078a43966921525bddf90f3bc54407de7
Akhil Siva Chowdary Nandigam <akhilsivachowdary11@gmail.com>
2025-12-09 13:18:01 +0530

    Fix ALLOWED_HOSTS formatting in settings.py

--- gradeX/settings.py
+++ gradeX/settings.py
@@ -28,7 +28,7 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 #ALLOWED_HOSTS = ["AkhilSivaChowdary.pythonanywhere.com"]
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gradeX.onrender.com', ''gradex-ogwk.onrender.com'']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'gradeX.onrender.com', 'gradex-ogwk.onrender.com']
 
 
 
